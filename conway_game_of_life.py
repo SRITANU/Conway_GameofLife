521aca4fe61de05a17bb27b942c33dec8698ed00
Sritanu Chakraborty <sritanu25@gmail.com>
2015-01-21 15:28:34 +0530

    Adds the functions to generate the next generation patterns.

--- conway_game_of_life.py
+++ conway_game_of_life.py
@@ -24,46 +24,46 @@ import random
 import time
 
 def generate_random_pattern(cols, rows, pattern_array):
-	""" 
+	"""
 	This function generates and returns a random patterned array,if empty.Otherwise,
 	it substitutes '.' in place of 0(dead cells) and '*' in place of 1
 	(live cells).
 	"""
-    
 
-    #When the program is run for the first time the pattern array is empty
-    if pattern_array == []:
-        for i in range(0,rows):
-            pat_row = []
-            for j in range(0,cols):
-            	#The cells on the top,right,bottom,left cells are always dead
-                if (i == 0 or j == 0 or (i == rows - 1) or (j == cols - 1)):
-                    pat_row += ['.']
-                else:
-                #Generates a random integer either 1 or 0.
-                #If its 0 then the pattern is '.',if its 1 then its '*'.	
-                    r = random.randint(0,1)
-                    if r == 0:
+
+        #When the program is run for the first time the pattern array is empty
+        if pattern_array == []:
+            for i in range(0,rows):
+                pat_row = []
+                for j in range(0,cols):
+            	    #The cells on the top,right,bottom,left cells are always dead
+                    if (i == 0 or j == 0 or (i == rows - 1) or (j == cols - 1)):
                         pat_row += ['.']
                     else:
-                        pat_row += ['*']
-            pattern_array += [pat_row]
-    #When the program is run for second or more generations,the pattern array
-    #constitues of zeros and ones which are converted to '*' and '.' respectively
-    else:
-        for i in range(0,rows):
-            for j in range(0,cols):
-            	#The cells on the top,right,bottom,left cells are always dead
-                if (i == 0 or j == 0 or (i == rows - 1) or (j == cols - 1)):
-                    pattern_array[i][j]= '.'
-                else:
-                	#If its 0 then the pattern is '.',if its 1 then its '*'.
-                    if pattern_array[i][j] == 0:
+                    #Generates a random integer either 1 or 0.
+                    #If its 0 then the pattern is '.',if its 1 then its '*'.
+                        r = random.randint(0,1)
+                        if r == 0:
+                            pat_row += ['.']
+                        else:
+                            pat_row += ['*']
+                pattern_array += [pat_row]
+        #When the program is run for second or more generations,the pattern array
+        #constitues of zeros and ones which are converted to '*' and '.' respectively
+        else:
+            for i in range(0,rows):
+                for j in range(0,cols):
+            	    #The cells on the top,right,bottom,left cells are always dead
+                    if (i == 0 or j == 0 or (i == rows - 1) or (j == cols - 1)):
                         pattern_array[i][j]= '.'
                     else:
-                        pattern_array[i][j]= '*'
+                	#If its 0 then the pattern is '.',if its 1 then its '*'.
+                        if pattern_array[i][j] == 0:
+                            pattern_array[i][j]= '.'
+                        else:
+                            pattern_array[i][j]= '*'
 
-    return pattern_array
+        return pattern_array
 
 
 def print_twodimensional_pattern(pattern_array):
@@ -88,20 +88,75 @@ def pattern_to_binary_converter(cols, rows, pattern_array):
 	"""
 
 	for i in range(rows):
-        for j in range(cols):
-            if (i == 0 or j == 0 or (i == rows - 1) or (j == cols - 1)):
-                pattern_array[i][j] = "x"
-            elif array[i][j] == '.':
-                pattern_array[i][j] = 0
-            else:
-                pattern_array[i][j] = 1
-    return pattern_array
+            for j in range(cols):
+                if (i == 0 or j == 0 or (i == rows - 1) or (j == cols - 1)):
+                    pattern_array[i][j] = "x"
+                elif pattern_array[i][j] == '.':
+                    pattern_array[i][j] = 0
+                else:
+                    pattern_array[i][j] = 1
+        return pattern_array
 
 
-def next_generation_pattern_generator(cols, rows, current_generation, next_generation):
+def next_generation_pattern_generator(cols, rows, binary_array, next_generation):
 	"""
-	Generates the next generation pattern array based on the rules of the game.
+	This function generates and returns the next generation pattern array based on
+	the rules of the game.
 	"""
+
+	#List comprehension to initiate the next_generation_pattern with zeros
+	next_generation = [([0] * cols) for row in range(rows)]
+	#Need to process only the cells except the outside most cells 
+	for i in range(1,rows-1):
+            for j in range(1,cols-1):
+                next_generation[i][j] = count_and_process_neighbours(i, j, binary_array)
+
+        return next_generation
+
+def count_and_process_neighbours(a,b,current_pattern):
+	"""
+	This function counts the neighbours of a particular cell,decides the value of 
+	the cell for the next_generation pattern and returns it to the next_generation
+	_pattern_generator function.
+
+	The following are the rules:
+	1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+	2. Any live cell with more than three live neighbours dies, as if by overcrowding.
+	3. Any live cell with two or three live neighbours lives on to the next generation.
+	4. Any dead cell with exactly three live neighbours becomes a live cell.
+
+	A particular cells has eight cells in a Moore neighbourhood,as illustrated in the following
+	diagram:
+
+	(i-1,j-1) | (i-1,j) | (i-1,j+1)
+	-------------------------------
+	 (i,j-1)  |  (i,j)  |  (i,j+1)
+	-------------------------------
+	(i+1,j-1) | (i+1,j) | (i+1,j+1)
+
+	So,the two loops runs from (i-1) to (i+1) and (j-1) to (j+1) to determine the postion and value
+	of the neighbourhood cells,considering the fact that the own value of a particular cell should 
+        not be taken into account while counting and processing the neighbourhood cells.
+	"""
+
+	count = 0
+        for j in range(b-1,b+2):
+            for i in range(a-1,a+2):
+                if not(i == a and j == b):
+                    if current_pattern[i][j] != 'x':
+                        count += current_pattern[i][j]
+        #Condition 1
+        if current_pattern[a][b] == 1 and count < 2:
+            return 0
+        #Condition 2
+        if current_pattern[a][b] == 1 and count > 3:
+            return 0
+        #Condition 4
+        if current_pattern[a][b] == 0 and count == 3:
+            return 1
+        #Condition 3
+        else:
+            return current_pattern[a][b]
 
 
 def main():
@@ -111,23 +166,23 @@ def main():
 	"""
 
 
-    print "Welcome to Conway's Game of Life"
-    print "================================"
-    rows = int(raw_input("Enter the no.of rows > "))
-    columns = int(raw_input("Enter the no.of columns > "))
-    generations = int(raw_input("Enter the no.of generations > "))
-    time_delay = 0.2
-    current_generation = []
-    next_generation = []
+        print "Welcome to Conway's Game of Life"
+        print "================================"
+        rows = int(raw_input("Enter the no.of rows > "))
+        columns = int(raw_input("Enter the no.of columns > "))
+        generations = int(raw_input("Enter the no.of generations > "))
+        time_delay = 0.2
+        current_generation = []
+        next_generation = []
 
 
-    for i in range(generations):
-        print "Conway's Game of Life -- Generation " + str(i+1)
-        print_twodimensional_pattern(generate_random_pattern(columns, rows, current_generation))
-        binary_array = pattern_to_binary_converter(columns, rows, current_generation)
-        next_generation = next_generation_pattern_generator(columns, rows, binary_array, next_generation)
-        time.sleep(time_delay)
-        current_generation, next_generation = next_generation, current_generation
+        for i in range(generations):
+            print "Conway's Game of Life -- Generation " + str(i+1)
+            print_twodimensional_pattern(generate_random_pattern(columns, rows, current_generation))
+            binary_array = pattern_to_binary_converter(columns, rows, current_generation)
+            next_generation = next_generation_pattern_generator(columns, rows, binary_array, next_generation)
+            time.sleep(time_delay)
+            current_generation, next_generation = next_generation, current_generation
 
 if __name__ == '__main__':
     main()
